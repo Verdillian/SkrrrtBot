56bd2ffde4b343cfe3c203726fdf3711c39e7e4e
byBlurr <dbldumont11@gmail.com>
2017-12-22 23:40:47 +0000

    Tidy up again

---
+++ KnightBot/Modules/Public/ImagesModule.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+using System.Linq;
+using KnightBot.Config;
+using KnightBot.util;
+using System.Net.Http;
+using System.Net;
+using Newtonsoft.Json.Linq;
+
+namespace KnightBot.Modules.Public
+{
+    public class ImagesModule : ModuleBase
+    {
+        Errors errors = new Errors();
+
+        private ImageSharp.Image image = null;
+
+        [Command("doggo")]
+        [Summary("Posts random doggo pictures!")]
+        public async Task Dog()
+        {
+            Console.WriteLine("Making API Call...");
+            using (var client = new HttpClient(new HttpClientHandler { AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate }))
+            {
+                string websiteurl = "https://random.dog/woof.json";
+                client.BaseAddress = new Uri(websiteurl);
+                HttpResponseMessage response = client.GetAsync("").Result;
+                response.EnsureSuccessStatusCode();
+                string result = await response.Content.ReadAsStringAsync();
+                var json = JObject.Parse(result);
+
+
+                Random randal;
+                string[] doggers;
+                randal = new Random();
+                doggers = new string[]
+                {
+                        "doggers/dog1.jpeg",
+                        "doggers/dog2.jpg",
+                        "doggers/dog3.jpg",
+                        "doggers/dog4.jpg",
+                        "doggers/dog5.jpg",
+                        "doggers/dog6.jpg",
+                        "doggers/dog7.jpg",
+                        "doggers/dog8.jpg",
+                        "doggers/dog9.jpg",
+                        "doggers/dog10.jpg",
+                };
+                string DogImage = json["url"].ToString();
+                if (DogImage.Contains("mp4"))
+                {
+                    int RandomDoggoIndex = randal.Next(doggers.Length);
+                    string DoggerToPost = doggers[RandomDoggoIndex];
+                    await Context.Channel.SendFileAsync(DoggerToPost);
+                }
+                else
+                {
+                    await ReplyAsync(DogImage);
+                }
+            }
+            await Context.Message.DeleteAsync();
+        }
+
+        [Command("cat")]
+        [Summary("Posts random cat pictures!")]
+        public async Task Cat()
+        {
+            Console.WriteLine("Making API Call...");
+            using (var client = new HttpClient(new HttpClientHandler { AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate }))
+            {
+                string websiteurl = "http://random.cat/meow";
+                client.BaseAddress = new Uri(websiteurl);
+                HttpResponseMessage response = client.GetAsync("").Result;
+                response.EnsureSuccessStatusCode();
+                string result = await response.Content.ReadAsStringAsync();
+                var json = JObject.Parse(result);
+                string CatImage = json["file"].ToString();
+                await ReplyAsync(CatImage);
+            }
+            await Context.Message.DeleteAsync();
+        }
+    }
+}
--- KnightBot/Modules/Public/PublicModule.cs
+++ KnightBot/Modules/Public/PublicModule.cs
@@ -16,7 +16,6 @@ namespace KnightBot.Modules.Public
     {
         Errors errors = new Errors();
 
-        private ImageSharp.Image image = null;
         private string randomString = "";
 
         // DM Command - Sends a message directly to knight.
@@ -55,71 +54,6 @@ namespace KnightBot.Modules.Public
 
             await Context.Channel.SendMessageAsync("", false, embed);
 
-            await Context.Message.DeleteAsync();
-        }
-        
-        [Command("doggo")]
-        [Summary("Posts random doggo pictures!")]
-        public async Task Dog()
-        {
-            Console.WriteLine("Making API Call...");
-            using (var client = new HttpClient(new HttpClientHandler { AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate }))
-            {
-                string websiteurl = "https://random.dog/woof.json";
-                client.BaseAddress = new Uri(websiteurl);
-                HttpResponseMessage response = client.GetAsync("").Result;
-                response.EnsureSuccessStatusCode();
-                string result = await response.Content.ReadAsStringAsync();
-                var json = JObject.Parse(result);
-
-
-                Random randal;
-                string[] doggers;
-                randal = new Random();
-                doggers = new string[]
-                {
-                        "doggers/dog1.jpeg",
-                        "doggers/dog2.jpg",
-                        "doggers/dog3.jpg",
-                        "doggers/dog4.jpg",
-                        "doggers/dog5.jpg",
-                        "doggers/dog6.jpg",
-                        "doggers/dog7.jpg",
-                        "doggers/dog8.jpg",
-                        "doggers/dog9.jpg",
-                        "doggers/dog10.jpg",
-                };
-                string DogImage = json["url"].ToString();
-                if (DogImage.Contains("mp4"))
-                {
-                    int RandomDoggoIndex = randal.Next(doggers.Length);
-                    string DoggerToPost = doggers[RandomDoggoIndex];
-                    await Context.Channel.SendFileAsync(DoggerToPost);
-                }
-                else
-                {
-                    await ReplyAsync(DogImage);
-                }
-            }
-            await Context.Message.DeleteAsync();
-        }
-
-        [Command("cat")]
-        [Summary("Posts random cat pictures!")]
-        public async Task Cat()
-        {
-            Console.WriteLine("Making API Call...");
-            using (var client = new HttpClient(new HttpClientHandler { AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate }))
-            {
-                string websiteurl = "http://random.cat/meow";
-                client.BaseAddress = new Uri(websiteurl);
-                HttpResponseMessage response = client.GetAsync("").Result;
-                response.EnsureSuccessStatusCode();
-                string result = await response.Content.ReadAsStringAsync();
-                var json = JObject.Parse(result);
-                string CatImage = json["file"].ToString();
-                await ReplyAsync(CatImage);
-            }
             await Context.Message.DeleteAsync();
         }
 
